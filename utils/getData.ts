7b3e9ba4737c0844fc4ab8d7787d324a4c6dfca3
Lukie Kang <lukiek@gmail.com>
2023-12-13 11:23:42 +0000

    fix: undefined error in getData

--- utils/getData.ts
+++ utils/getData.ts
@@ -1,7 +1,8 @@
 import { ApiEndpoint } from "@/types"
 
+const baseUrl = process.env.API_URL || ''
 export async function getData(endpoint: ApiEndpoint) {
-    const res = await fetch(process.env.API_URL + endpoint)
+    const res = await fetch(baseUrl + endpoint)
     if (!res.ok) {
       throw new Error('Failed to fetch data')
     }
